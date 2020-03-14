1b1ab7f16c887e74ac49d069d5c75faddd9733bc
Eric <ericropiak@gmail.com>
2020-03-14 11:55:14 -0400

    adding barebones flask

---
+++ app/app.py
@@ -0,0 +1,11 @@
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+	return 'hi'
+
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=True, host="0.0.0.0")
