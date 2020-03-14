224c51f15390108f99bf6cf1bfb425c1ba18ae9e
Eric <ericropiak@gmail.com>
2020-03-14 18:53:10 -0400

    now postgres works locally

--- app/views/main.py
+++ app/views/main.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 from app.models import db, Person
 
+import sys
+
 main = Blueprint("main", __name__)
 
 @main.route("/")
@@ -8,4 +10,7 @@ def index():
 
     print(Person.query.all())
     print("Hello World!")
+    print("wowowo")
+    sys.stdout.flush() # Hack until logger is implemented
     return "<h1>Hello World!</h1>"
+
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,5 @@
-from app.flask_app import app as application
+from app.main import app as application
 
 # Used to serve application with gunicorn
 if __name__ == "__main__":
-	print('running gunic')
     application.run()
