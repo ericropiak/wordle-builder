4a5857db02e7e6d00ec40cf7a7bf70bc7d8f5fbe
Eric <ericropiak@gmail.com>
2020-03-14 14:33:14 -0400

    moving flask app into app directory

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
-from flask_app import app as application
+from app.flask_app import app as application
 
+# Used to serve application with gunicorn
 if __name__ == "__main__":
     application.run()
