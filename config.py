4ee2568d27e5aeec61566d8de64e4f02c1b7dc57
Eric <ericropiak@gmail.com>
2020-09-07 14:44:28 -0400

    increase gunicorn process count

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 # Due to the limited load balancing algorithm used by gunicorn, 
 # it is not possible to use more than one worker process when using socketio
-workers = int(os.environ.get('GUNICORN_PROCESSES', '1'))
+workers = int(os.environ.get('GUNICORN_PROCESSES', '3'))
 threads = int(os.environ.get('GUNICORN_THREADS', '1'))
 
 worker_class = 'eventlet'
