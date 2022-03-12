5417a0b923b69731b4851e7f14d6db4b891d276b
Eric Ropiak <ericropiak@gmail.com>
2022-03-11 20:28:03 -0500

    update prod DOckerfile

--- scripts/docker_start.sh
+++ scripts/docker_start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# LOCAL GUNICORN TEST:
 # gunicorn --chdir app --worker-class eventlet -w 1 run_local:app -b 0.0.0.0:5001
+# PROD IMITATION:
+# gunicorn --chdir app --worker-class eventlet -w 1 wsgi:application -b 0.0.0.0:5001
 python run_local.py
